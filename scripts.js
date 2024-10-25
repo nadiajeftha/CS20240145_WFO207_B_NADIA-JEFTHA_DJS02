4de67715e12f023b436c4445662a8e263ea435bb
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-25 15:00:08 +0200

    NaN

--- scripts.js
+++ scripts.js
@@ -6,6 +6,12 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
+  if (isNaN(dividend) || isNaN(divider)) {
+    result.innerHTML = "Something critical went wrong. Please reload the page.";
+    console.error("Call stack:", new Error().stack);
+    return;
+  }
+
   if (!dividend || !divider) {
     result.innerHTML =
       "Division not performed. Both values are required in inputs. Try again.";
