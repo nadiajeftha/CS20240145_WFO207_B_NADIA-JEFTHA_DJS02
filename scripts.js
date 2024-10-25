a3118d8f2b10fec71bac361edaf92cfb84d2a4ee
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-10-25 14:15:51 +0200

    START OVER

--- scripts.js
+++ scripts.js
@@ -1,19 +1,9 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-const dividendInputOnForm = document.getElementById("dividend");
-const dividerInputOnForm = document.getElementById("divider");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = parseInt(dividend / divider);
-});
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (!divider || !dividend) {
-    alert "Division not performed.Both values are required in inputs.Try again.";
-    return
-  }
+  result.innerText = dividend / divider;
 });
